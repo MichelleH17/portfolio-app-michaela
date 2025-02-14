ed13911dd7dadf7444b5cb1227ccc8f72de3a106
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-14 12:42:09 +0100

    feat: restructure app by adding NuxtLayout component and default layout file

--- app.vue
+++ app.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <Navbar />
-    <NuxtPage />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class=" container max-w-4xl p-4 mx-auto">
+    <Navbar />
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>

c62b7f3129c3737a60852ebd819ed7356774cd89
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-14 17:02:44 +0100

    style: add hover effects and updated font styles

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="border border-gray-100 shadow-md rounded-md px-2 md:px-4 py-4 md:py-8">
-    <NuxtLink :to="`/project/${project.id}`" class="hover:bg-custom_green px-4 py-2 rounded-md whitespace-nowrap">
-      <h3 class="text-xl font-semibold mb-2">{{ project.title }}</h3>
-      <p class="text-gray-700 mb-4">{{ project.summary }}</p>
+  <div class="hover:border-custom_green hover:bg-custom_gray hover:shadow-custom_darker_green border border-gray-100 shadow-md rounded-md px-2 md:px-4 py-4 md:py-8">
+    <NuxtLink :to="`/project/${project.id}`" class="px-4 py-2 rounded-md whitespace-nowrap">
+      <h3 class="text-xl font-semibold mb-2 font-Ovo">{{ project.title }}</h3>
+      <p class="text-gray-700 font-Outfit">{{ project.summary }}</p>
     </NuxtLink>
   </div>
 </template>

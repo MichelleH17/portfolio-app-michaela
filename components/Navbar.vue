02e12fc7418d91a825bf24fac8d474601ffc8b8c
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-14 10:23:26 +0100

    feat: create Navbar component with navigation links

---
+++ components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <header class="p-4 md:p-8">
+      <nav class="container max-w-4xl mx-auto border border-gray-100 shadow-md rounded-md px-2 md:px-4 py-4 md:py-8">
+        <ul class="flex gap-2 sm:gap-10 md:gap-20 justify-center font-semibold text-primary_text">
+          <li><NuxtLink to="/projects" class="hover:bg-custom_green px-4 py-2 rounded-md whitespace-nowrap">My projects</NuxtLink></li>
+          <li><NuxtLink to="/" class="hover:bg-custom_green px-4 py-2 rounded-md whitespace-nowrap">Home</NuxtLink></li>
+          <li><NuxtLink to="/about" class="hover:bg-custom_green px-4 py-2 rounded-md whitespace-nowrap">About me</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped>
+  .router-link-exact-active {
+    background-color: #c2deb9;
+    padding: 8px 16px;
+    border-radius: 6px;
+  }
+
+</style>

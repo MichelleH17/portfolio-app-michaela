f790a765272e2d4386fe18049ba0e31feb4a8671
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-14 12:06:17 +0100

    feat: add Profile component with links to GitHub and LinkedIn

---
+++ components/Profile.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="flex flex-col items-center mt-4 md:mt-8 text-custom_black px-4">
+      <NuxtImg fit="cover" format="webp" src="../public/assets/images/michaela_avatar.jpg" alt="Profile picture" width="400" height="400" class="rounded-full" />
+      <h2 class="text-custom_darker_green text-3xl font-semibold mt-4">Michaela Havlíková</h2>
+      <h3 class="text-lg text-custom_black">Frontend developer</h3>
+      <div class="flex flex-row gap-8 md:gap-16 mt-8">
+        <NuxtLink to="https://github.com/MichelleH17" target="_blank" rel="noopener noreferrer" class="bg-custom_green hover:bg-custom_darker_green hover:text-white w-32 text-center py-2 rounded-md font-semibold whitespace-nowrap flex items-center justify-center">
+          GitHub
+          <Icon name="uil:github" size="1.25em" style="background-color: custom_black" class="ml-1" />
+        </NuxtLink>
+        <NuxtLink to="https://www.linkedin.com/in/michaela-havlikova/" target="_blank" rel="noopener noreferrer" class="bg-custom_green hover:bg-custom_darker_green hover:text-white w-32 py-2 rounded-md font-semibold whitespace-nowrap flex items-center justify-center">
+          LinkedIn
+          <Icon name="uil:linkedin" size="1.25em" style="background-color: custom_black" class="ml-1" />
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>

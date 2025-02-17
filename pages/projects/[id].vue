7391af5669068c28c231e269ed6669b275feb1b6
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-17 12:09:29 +0100

    feat: implement project modal with data fetching from JSON

--- pages/projects/[id].vue
+++ pages/projects/[id].vue
@@ -1,13 +1,12 @@
 <template>
-  <div>
-    
-  </div>
+  <ModalWindow  />
 </template>
 
 <script lang="ts" setup>
+  import projectData from '~/data/projects.json'
 
+  const id = useRoute().params.id
+  const project = computed(() => projectData.find((project) => project.id === +id)!)
+
+  const isOpen = ref(true)
 </script>
-
-<style>
-
-</style>

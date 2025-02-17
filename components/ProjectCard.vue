95ae61ef9eed25cf934e1e50f4708fb5b2e94e7a
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-17 12:10:48 +0100

    fix: edit styles at ProjectCard component

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="hover:border-custom_green hover:bg-custom_gray hover:shadow-custom_darker_green border border-gray-100 shadow-md rounded-md p-4 md:py-8">
-    <NuxtLink :to="`/project/${project.id}`" class="px-4 py-2 rounded-md">
-      <h3 class="text-xl font-semibold mb-2 font-Ovo">{{ project.title }}</h3>
-      <p class="text-gray-700 font-Outfit">{{ truncateDescription(project.description) }}</p>
-    </NuxtLink>
+  <div class="hover:border-custom_green hover:bg-custom_gray hover:shadow-custom_darker_green border border-gray-100 shadow-md rounded-md p-4 font-Outfit">
+    <div class="flex flex-col space-y-4">
+      <h3 class="text-xl font-semibold">{{ project.title }}</h3>
+      <p class="text-gray-700">{{ truncateDescription(project.description) }}</p>
+      <NuxtLink :to="`/projects/${ project.id}`"
+        class="rounded-md bg-custom_green px-4 py-2 text-sm w-fit whitespace-nowrap font-medium text-custom_black hover:bg-custom_darker_green"
+      >
+        More information
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+  import projectData from '~/data/projects.json'
+
   defineProps<{ 
     project: IProject
   }>()
+
   const truncateDescription = (description: string) => {
     return description.length > 90 ? `${description.slice(0, 90)}...` : description
   }
 </script>
-
-<style>
-
-</style>

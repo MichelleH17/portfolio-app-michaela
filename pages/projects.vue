9de3bcc97ef72a012dcfb887b86da35972a65338
Michaela Havlíková <Michelle.h@seznam.cz>
2025-02-17 12:06:05 +0100

    feat: create projects page with project cards and data import

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="mt-4 sm:mt-8 md:mt-16 md:px-8 grid grid-cols-1 md:grid-cols-2 gap-4">
     <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
+    <NuxtPage />
   </div>
 </template>
 
 <script lang="ts" setup>
-  import projectData from '~/projects.json'
+  import projectData from '~/data/projects.json'
   
   const projects = ref(projectData)
 </script>
